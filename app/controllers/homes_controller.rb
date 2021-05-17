e3ae41f3dbbad67480757570c2000af4a92b05f0
obara134 <tyobara@icloud.com>
2021-05-17 10:33:26 +0000

    update

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,7 @@
 class HomesController < ApplicationController
 
   def top
-    @events = Event.where("start_at > ?", Time.zone.now).order(:start_at).page(params[:page]).reverse_order
+    @events = Event.where("start_at > ?", Time.zone.now).order(:start_at).page(params[:page])
   end
 
   def about
--- app/controllers/ticket_comments_controller.rb
+++ app/controllers/ticket_comments_controller.rb
@@ -5,12 +5,14 @@ class TicketCommentsController < ApplicationController
     comment = current_user.ticket_comments.new(ticket_comment_params)
     comment.event_id = event.id
     comment.save
+    flash[:notice] = "このイベントへの参加申し込みが完了しました"
     redirect_to event_path(event.id)
   end
 
   def destroy
     current_user.ticket_comments.where(event_id: params[:event_id]).destroy_all
     # TicketComment.find_by(id: params[:id], event_id: params[:event_id]).destroy
+    flash[:notice] = "このイベントへの参加申し込みを取り消しました"
     redirect_to event_path(params[:event_id])
   end
 
