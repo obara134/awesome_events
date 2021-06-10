d11d0534264cc2cda51dd6f438d478b2bdfa2ff4
obara134 <tyobara@icloud.com>
2021-06-10 16:07:54 +0000

    イベントの開催時間と終了時間のバリデーション

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,6 +8,16 @@ class Event < ApplicationRecord
   validates :introduction, length: { maximum: 2000 }, presence: true
   validates :start_at, presence: true
   validates :end_at, presence: true
+  validate :start_end_check
+  validate :start_check
+
+  def start_end_check
+    errors.add(:end_at, "は開始時刻より遅い時間を選択してください") if start_at > end_at
+  end
+
+  def start_check
+    errors.add(:start_at, "は現在の日時より遅い時間を選択してください") if start_at < Time.now
+  end
 
   def self.search_for(content) #検索機能
     Event.where('name LIKE ?', '%' + content + '%')
